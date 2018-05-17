746a0a6ae5265af24d676c4da119cfeab3145d2e
Varun Rustomji <varun0248@gmail.com>
2018-05-16 23:19:20 -0700

    Delete secrets.py

--- secrets.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'X5TMAg8yCKAjvt3gFqtl6a7sr'
-consumer_secret = 'UQiWJHPmxjChwaGeglldhJ66ru6SoTdkwxelgzeWWQsFu1g5VP'
-access_token = '992498730354929664-beAJ8m2VxloQfSqwhTKkIOLmN5vrsHI'
-access_secret = 'RlIWy3XWArVz47Y3A0Tsos2jddqNcdbVSbclfxJg2XOH1'
